72f5822713288863763f4ba3764ba853fb7883fb
Progyan <progyan.das@iitgn.ac.in>
2022-06-02 20:27:15 +0530

    setup: added setup.py and __init__.py to make pip installable.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='wimpyML',
+      version='0.1A',
+      description='Very wimpy implementations of Machine Learning algorithms.',
+      url='http://github.com/plugyawn/wimpyml',
+      author='Progyan Das',
+      author_email='progyan.das@iitgn.ac.in',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      )
